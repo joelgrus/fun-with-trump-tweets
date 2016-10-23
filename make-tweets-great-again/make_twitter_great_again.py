5fda3b06196f9fe307fdcca1bb203b160970a305
Joel Grus <joelgrus@gmail.com>
2016-10-23 13:28:56 -0700

    make twitter great again

---
+++ make-tweets-great-again/make_twitter_great_again.py
@@ -0,0 +1,29 @@
+from twython import Twython
+from twython.exceptions import TwythonError
+import json, os, re
+
+with open('credentials.json') as f:
+    creds = json.loads(f.read())
+
+client = Twython(
+    creds['consumer_key'],
+    creds['consumer_secret'],
+    creds['access_token_key'],
+    creds['access_token_secret']
+)
+
+query = 'make "great again" -america -filter:retweets'
+rgx = r"make (.*) great again"
+
+results = client.search(q=query)
+for tweet in results["statuses"]:
+  text = tweet["text"]
+  if re.search(rgx, text, re.I):
+    # twitter.retweet will raise an error if we try to retweet a tweet
+    # that we've already retweeted.
+    print(tweet)
+    try:
+      client.retweet(id=tweet["id"])
+      break
+    except TwythonError as e:
+      print(e)
